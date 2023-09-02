0e7cfe31ef90dea864b445986e93d42359316b00
uncvpn <133527215+uncvpn@users.noreply.github.com>
2023-09-02 11:09:23 +0700

    Update unc.php

--- unc.php
+++ unc.php
@@ -5,3 +5,9 @@
 แอพพลิเคชั่นอยุ่ระหว่างการปรับปรุง 
 หากผู้ใช้ทำการอัปเดต
 จะทำให้เข้าใช้งานไม่ได้นะคับ
+
+Don't press update
+Let everyone press cancel
+or press the cross symbol
+application is currently under development system
+If the user presses update will not be able in app

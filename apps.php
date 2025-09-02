813394fc57c80f8f44dfb0c1271ef230a8c2a691
opunc <uncuncunc99@gmail.com>
2025-09-03 00:37:51 +0700

    Update apps.php

--- apps.php
+++ apps.php
@@ -1,1 +1,7 @@
-
+{
+  "Boolean": false,
+  "GooglePlay": "https://play.google.com/store/apps/details?id=com.publicreggaevpn.reggaevpn",
+  "Message": "โปรดอ่านรายละเอียด
+แอปพลิเคชั่นปิดให้บริการชั่วคราว 
+กรุณากดติดตั้งเพื่อย้ายไปยังแอปพลิเคชั่นตัวใหม่"
+}
